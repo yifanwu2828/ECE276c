60f94ca665eab4cbf1d6a4867b80137c11369c6c
yifanwu2828 <yiw084@ucsd.edu>
2021-09-28 21:11:16 -0700

    update

--- HW1/DoF_Arm.py
+++ HW1/DoF_Arm.py
@@ -1,7 +1,11 @@
 import time
 
 import numpy as np
+import scipy.linalg as la
 import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use("TkAgg")
 
 import gym
 import pybulletgym
@@ -57,8 +61,8 @@ if __name__ == "__main__":
     # env.render(mode="human")
     env.reset()
 
-    print(f"observation space: {env.observation_space}")
-    print(f"action space: {env.action_space}")
+    # print(f"observation space: {env.observation_space}")
+    # print(f"action space: {env.action_space}")
 
     q0, q0_dot = env.unwrapped.robot.central_joint.current_position()
     ic(q0, q0_dot)
@@ -68,12 +72,39 @@ if __name__ == "__main__":
 
     x, y = getForwardModel(q0, q1)
     ic(x, y)
-
+    
+    
+    T = 360
     X_ref, Y_ref = [], []
-    for theta in np.linspace(-np.pi, np.pi, 360):
-        x_ref, y_ref = get_reference_trajectory(theta)
+    theta = np.linspace(-np.pi, np.pi, T)
+    e = np.zeros((T, 2))
+    Kp = 1
+    Ki = 0
+    Kd = 0
+    for t in range(0, T):
+        x_ref, y_ref = get_reference_trajectory(theta[t])
         X_ref.append(x_ref)
         Y_ref.append(y_ref)
+        
+        x_error = x_ref - x
+        y_error = y_ref - y
+        e[t, :] = [x_error, y_error]
+        
+        if t == 0:
+            continue
+        
+        ux_t = Kp * e[t, 0] + Ki + Kd * (e[t, 0] - e[t-1, 0])
+        uy_t = Kp * e[t, 1] + Ki + Kd * (e[t, 1] - e[t-1, 1])
+        
+        
+                
+        J = getJacobian(q0, q1)
+        J_inv, rank = la.pinv(J, return_rank=True)
+        assert rank == J.shape[1], "Jacobian matrix is singular"
+        delta_q = J_inv @ e[t, :]
+        # ic(delta_q)
+        # import ipdb; ipdb.set_trace()
+                
     plt.plot(X_ref, Y_ref)
     plt.xlim(-0.3, 0.3)
-    plt.show()
+    # plt.show()
