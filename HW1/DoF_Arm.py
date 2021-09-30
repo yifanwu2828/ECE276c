d20f3c40cdc4eaca7271654139f8ffaab0cb4339
yifanwu2828 <yiw084@ucsd.edu>
2021-09-29 23:02:17 -0700

    update

--- HW1/DoF_Arm.py
+++ HW1/DoF_Arm.py
@@ -1,11 +1,11 @@
 import time
+import pathlib
+from  typing import Tuple
+
 
 import numpy as np
 import scipy.linalg as la
 import matplotlib.pyplot as plt
-
-import matplotlib
-matplotlib.use("TkAgg")
 
 import gym
 import pybulletgym
@@ -13,98 +13,174 @@ import pybulletgym
 from icecream import ic
 
 
-l0 = 0.1
-l1 = 0.11
-
-
-def getForwardModel(q0, q1):
+class EzReacher(gym.ObservationWrapper):
     """
-    Returns the end-effector position given the joint angles.
-    :param q0: angle of joint 0
-    :param q1: angle of joint 1
-    :return: x, y postion of the end-effector
+    A custom env wrapper 
     """
-    x = l0 * np.cos(q0) + l1 * np.cos(q0 + q1)
-    y = l0 * np.sin(q0) + l1 * np.sin(q0 + q1)
-    return x, y
+
+    def __init__(self, env):
+        super().__init__(env)
+        self.l0 = 0.1
+        self.l1 = 0.11
+    
+    def observation(self, observation: np.ndarray) -> np.ndarray:
+        return observation
+    
+    def setJointPosition(self, q0, q1) -> None:
+        """
+        Takes in a joint position and returns the next state and reward
+        """
+        self.unwrapped.robot.central_joint.reset_position(q0, 0)
+        self.unwrapped.robot.elbow_joint.reset_position(q1, 0)
+    
+    def getCurrentJointPosition(self) -> np.ndarray:
+        """
+        Returns the current joint position and velocity
+        """
+        q0, q0_dot = self.unwrapped.robot.central_joint.current_position()
+        q1, q1_dot = self.unwrapped.robot.elbow_joint.current_position()
+        return q0, q0_dot, q1, q1_dot
+    
+    def getForwardModel(self, q0, q1) -> Tuple[float, float]:
+        """
+        Returns the end-effector position given the joint angles.
+        :param q0: angle of joint 0 in radians.
+        :param q1: angle of joint 1 in radians.
+        :return: x, y postion of the end-effector
+        """
+        x = self.l0 * np.cos(q0) + self.l1 * np.cos(q0 + q1)
+        y = self.l0 * np.sin(q0) + self.l1 * np.sin(q0 + q1)
+        return x, y
+
+    def getInverseKinematics(self, x, y) -> Tuple[float, float]:
+        """
+        Returns the inverse kinematics of the robot.
+        :param x: x position of the end-effector
+        :param y: y position of the end-effector
+        :return: joint angles q0 and q1 in radians
+        """
+        q1 = np.arccos((x**2 + y**2 - self.l0**2 - self.l1**2) / (2 * self.l0 * self.l1))
+        q0 = np.arctan2(y, x) - np.arctan2(self.l1 * np.sin(q1), self.l0 + self.l1 * np.cos(q1))
+        return q0, q1
+
+    def getJacobian(self, q0, q1) -> np.array:
+        """
+        Returns the Jacobian matrix of the forward model.
+        :param q0: angle of joint 0 in radians.
+        :param q1: angle of joint 1 in radians.
+        :return: Jacobian matrix
+        """
+        J = np.array(
+            [
+                [-self.l0 * np.sin(q0) - self.l1 * np.sin(q0 + q1), -self.l1 * np.sin(q0 + q1)],
+                [self.l0 * np.cos(q0) + self.l1 * np.cos(q0 + q1), self.l1 * np.cos(q0 + q1)],
+            ]
+        )
+        return J
 
 
-def getJacobian(q0, q1):
-    """
-    Returns the Jacobian matrix of the forward model.
-    :param q0: angle of joint 0
-    :param q1: angle of joint 1
-    :return: Jacobian matrix
-    """
-    J = np.array(
-        [
-            [-l0 * np.sin(q0) - l1 * np.sin(q0 + q1), -l1 * np.sin(q0 + q1)],
-            [l0 * np.cos(q0) + l1 * np.cos(q0 + q1), l1 * np.cos(q0 + q1)],
-        ]
-    )
-    return J
+class PDController:
+    def __init__(self, p: np.ndarray, d: np.ndarray, env: gym.Env):
+        self.Kp = np.diag([p, p])
+        self.Kd = np.diag([d, d])
+        self.env = env
+    
+    def end_effector_control(self, state_err, velocity_err, q0, q1):
+        # P control
+        delta_state = self.Kp @ state_err + self.Kd @ velocity_err
+                
+        J = self.env.getJacobian(q0, q1)
+        J_inv, rank = la.pinv(J, return_rank=True)
+        assert rank == J.shape[1], "Jacobian matrix is singular"
+        q = J_inv @ delta_state
+        return q
+        
 
-
-def get_reference_trajectory(theta):
+def get_reference_trajectory(num_steps, start= -np.pi, end= np.pi):
     """
     Returns the reference trajectory.
     :param theta: angle \in [-pi, pi]
-    :return: reference trajectory
+    :return: (x,y) of reference trajectory
     """
-    x_ref = (0.19 + 0.02 * np.cos(4 * theta)) * np.cos(theta)
-    y_ref = (0.19 + 0.02 * np.cos(4 * theta)) * np.sin(theta)
-    return x_ref, y_ref
+    angles = np.linspace(start, end, num_steps)
+    ref = np.empty((num_steps, 2))
+    for t, theta in enumerate(angles):
+        x_ref = (0.19 + 0.02 * np.cos(4 * theta)) * np.cos(theta)
+        y_ref = (0.19 + 0.02 * np.cos(4 * theta)) * np.sin(theta)
+        ref[t, :] = x_ref, y_ref
+    return ref
+
+def getReferenceVelocity(theta):
+    """
+    Get reference velocity
+    :param theta: float, theta
+    :return: x_dot, y_dot velocity of reference trajectory
+    """
+    x_dot = - np.sin(theta)*(np.cos(4*theta)/50 + 19/100) - \
+        (2*np.sin(4*theta)*np.cos(theta))/25
+    y_dot = np.cos(theta)*(np.cos(4*theta)/50 + 19/100) - \
+        (2*np.sin(4*theta)*np.sin(theta))/25
+    return x_dot, y_dot
 
 
 if __name__ == "__main__":
+    path = pathlib.Path(__file__).parent.resolve()
+
     env = gym.make("ReacherPyBulletEnv-v0")
+    env = EzReacher(env)
+    # env.seed(42)
     # env.render(mode="human")
     env.reset()
-
-    # print(f"observation space: {env.observation_space}")
-    # print(f"action space: {env.action_space}")
-
-    q0, q0_dot = env.unwrapped.robot.central_joint.current_position()
-    ic(q0, q0_dot)
-
-    q1, q1_dot = env.unwrapped.robot.elbow_joint.current_position()
-    ic(q1, q1_dot)
-
-    x, y = getForwardModel(q0, q1)
-    ic(x, y)
+    # PID parameters
+    p = 200
+    d = 10
+    contorller = PDController(p, d, env)
     
+    # Time step
+    T = 400
+    V_ref = 0
     
-    T = 360
-    X_ref, Y_ref = [], []
-    theta = np.linspace(-np.pi, np.pi, T)
-    e = np.zeros((T, 2))
-    Kp = 1
-    Ki = 0
-    Kd = 0
+    # -------------------------------------------------------------------------
+    # Save trajectory
+    # with open(path/"ref.npy", "wb") as f:
+        # np.save(f, get_reference_trajectory(T))
+    # -------------------------------------------------------------------------
+    
+    # Load reference trajectory
+    with open(path/"ref.npy", "rb") as f:
+        REF = np.load(f)
+        
+    # errors
+    e = np.empty_like((REF))
+    # real trajectory
+    traj = np.empty_like(REF)
+
+    # q0, q1 = env.getInverseKinematics(REF[0, 0], REF[0, 1])
+    # env.setJointPosition(q0, q1)
+    
     for t in range(0, T):
-        x_ref, y_ref = get_reference_trajectory(theta[t])
-        X_ref.append(x_ref)
-        Y_ref.append(y_ref)
+        q0, q0_dot, q1, q1_dot = env.getCurrentJointPosition()
+        cur_X = env.getForwardModel(q0, q1)
+        cur_J = env.getJacobian(q0, q1)        
+        cur_V = cur_J @ np.array([q0_dot, q1_dot])
+
+        state_err = REF[t, :] - cur_X
+        velocity_err = V_ref - cur_V
+            
+        q = contorller.end_effector_control(state_err, velocity_err, q0, q1)
         
-        x_error = x_ref - x
-        y_error = y_ref - y
-        e[t, :] = [x_error, y_error]
-        
-        if t == 0:
-            continue
-        
-        ux_t = Kp * e[t, 0] + Ki + Kd * (e[t, 0] - e[t-1, 0])
-        uy_t = Kp * e[t, 1] + Ki + Kd * (e[t, 1] - e[t-1, 1])
-        
-        
-                
-        J = getJacobian(q0, q1)
-        J_inv, rank = la.pinv(J, return_rank=True)
-        assert rank == J.shape[1], "Jacobian matrix is singular"
-        delta_q = J_inv @ e[t, :]
-        # ic(delta_q)
-        # import ipdb; ipdb.set_trace()
-                
-    plt.plot(X_ref, Y_ref)
+        env.step(q)
+        # time.sleep(0.1)
+        traj[t, :] = cur_X                               
+
+    MSE = np.mean((REF - traj)**2)
+    ic(MSE)
+
+    plt.plot(REF[:, 0], REF[:, 1], label="reference")
+    plt.scatter(REF[0, 0], REF[0, 1], marker='^', c='b', label="reference_start&end")
+    plt.scatter(traj[0, 0], traj[0, 1], c="r", label="start")
+    plt.scatter(traj[-1, 0], traj[-1, 1], c='g', label="end")
+    plt.plot(traj[:, 0], traj[:, 1], label="trajectory")
     plt.xlim(-0.3, 0.3)
-    # plt.show()
+    plt.legend()
+    plt.show()
